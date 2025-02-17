9931890a017805c786cfa5bb08e71b1cb73bc38a
Cristovan Tamayo <ctamayo@bionexo.com>
2025-02-17 08:16:03 -0300

    external bd config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,11 +7,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
       ssl: {
         rejectUnauthorized: false,
       },
