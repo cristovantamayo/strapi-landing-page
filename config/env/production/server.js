baa180bf5f9eeec46be44f51f4a75ef8a8bbcef3
Cristovan Tamayo <ctamayo@bionexo.com>
2025-02-19 06:46:10 -0300

    Keys as array

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,11 @@ module.exports = ({ env }) => ({
   proxy: true,
   url: env("https://strapi-landing-page-84e3fa2a7657.herokuapp.com/"), // Sets the public URL of the application.
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array([
+      "S+8Tfrt/Kss6tRY8kG+1Pg==",
+      "zVuFDznUMVuTqltMbTtLng==",
+      "JP39FOQwsdc6BnXLcOJIGQ==",
+      "la4vf3L9n/j8ztLYfVf8ug==",
+    ]),
   },
 });
