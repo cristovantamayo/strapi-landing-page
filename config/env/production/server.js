4d078f5b117203a615f51b50a63ed4f3539f44f4
Cristovan Tamayo <ctamayo@bionexo.com>
2025-02-19 07:23:26 -0300

    only envs

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,13 +1,8 @@
 // Path: ./config/env/production/server.js`
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("https://strapi-landing-page-84e3fa2a7657.herokuapp.com/"), // Sets the public URL of the application.
+  url: env("MY_HEROKU_URL"),
   app: {
-    keys: env.array([
-      "S+8Tfrt/Kss6tRY8kG+1Pg==",
-      "zVuFDznUMVuTqltMbTtLng==",
-      "JP39FOQwsdc6BnXLcOJIGQ==",
-      "la4vf3L9n/j8ztLYfVf8ug==",
-    ]),
+    keys: env.array("APP_KEYS"),
   },
 });
