42991f6b0e7bd63c54877428b256669b9095e34e
Cristovan Tamayo <ctamayo@bionexo.com>
2025-02-22 07:45:38 -0300

    database cursor refactor

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,34 +1,22 @@
 const path = require("path");
 
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: true,
+      },
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-  };
-};
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
+  },
+});
